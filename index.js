74f51f556f626b2c15d2ffb8ba5ca40f0a25cc0d
Alexandros Raikos <alexandros@dood.gr>
2023-02-24 17:54:23 +0200

    created main class

--- index.js
+++ index.js
@@ -1,5 +1,40 @@
-export { Binding } from './src/model/binding'
-export { Modal } from './src/components/modal'
-export { notify } from './src/functions/base'
-export { CommunicationController } from './src/controllers/communication-controller'
-export { InteractionController } from './src/controllers/interaction-controller'
+import Modal from './src/components/modal'
+import API from './src/controllers/communication-controller'
+import InteractionController from './src/controllers/interaction-controller'
+import { notify } from './src/functions/base'
+import Binding from './src/model/binding'
+
+class Gin {
+    constructor() {
+      this.api = this.api();
+      this.interface = this.interface();
+    }
+  
+    binding(eventType, actionID, method) {
+        return new Binding(eventType, actionID, method)
+    }
+
+    api() {
+        return new API()
+    }
+
+    interface() {
+        return new InteractionController()
+    }
+
+    notify(message, type = 'error', disappearing = true) {
+        notify(message, type, disappearing)
+    }
+
+    modal(
+        identifier,
+        data,
+        index = 0,
+        completion = null,
+        fullCanvas = false
+    ) {
+        return new Modal(identifier, data, index, completion, fullCanvas)
+    }
+}
+
+module.exports = Gin;
